b1bc2306b210cfa01cf800a3cb0a81a1a0fc0373
Botirjon <sobirovumidjonobidjonovich@gmail.com>
2026-04-10 18:31:55 +0500

    edited responsive

--- src/components/PartnerBrand/index.jsx
+++ src/components/PartnerBrand/index.jsx
@@ -30,7 +30,7 @@ export default function index({ mode }) {
       {/* Content */}
       <div className="flex flex-col items-center container w-full max-w-[1200px] px-2 mx-auto ">
         <h2
-          className={`text-center text-[50px] font-bold w-full max-w-[700px] ${mode === true ? "text-white" : "text-black"}`}
+          className={`text-center lg:text-[50px] md:text-[40px]  font-bold w-full max-w-[700px] ${mode === true ? "text-white" : "text-black"}`}
         >
           Biz ishlagan Brendlar
         </h2>
@@ -41,12 +41,14 @@ export default function index({ mode }) {
       </div>
       {/* Brands images */}
       <div>
-        <ul className="flex items-center justify-between bg-gray-800 gap-8 w-max marquee mt-[30px]">
+        <ul
+          className={`flex items-center bg-gray-800 w-max marquee mt-[30px] lg:gap-8 md:gap-6 sm:gap-5 gap-4`}
+        >
           {images.map((image, index) => {
             return (
               <li className="list-none" key={index}>
                 <img
-                  className={`${image === "honor-logo" || image === "infinix-logo" || image === "realme-logo" || image === "oppo-logo" || image === "vivo-logo" || image === "samsung-logo" ? "w-[150px]" : "w-[55px]"}`}
+                  className={`${image === "honor-logo" || image === "infinix-logo" || image === "realme-logo" || image === "oppo-logo" || image === "vivo-logo" || image === "samsung-logo" ? "lg:w-[150px] md:w-[125px] sm:w-[105px] w-[80px]" : "lg:w-[55px] md:w-[45px] sm:w-[35px] w-[30px]"}`}
                   src={`/${image}.png`}
                   alt={`${image} logo`}
                 />
