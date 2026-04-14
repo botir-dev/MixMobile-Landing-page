d8b180acf44d5b395a90eb8b316b82fe0a4d7160
Botirjon <sobirovumidjonobidjonovich@gmail.com>
2026-04-14 14:18:14 +0500

    footer responsive tugamagan ekan

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ export default function App() {
         <WhyChoose mode={theme} />
         <GetInTouch mode={theme} />
       </main>
-
       <footer>
         <Footer mode={theme} />
       </footer>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,8 +10,8 @@ export default function index({ mode }) {
     >
       <div className="container w-full max-w-[1200px] px-2 mx-auto py-15">
         {/* Logo */}
-        <div className="flex w-full justify-between">
-          <div className="w-[24%]">
+        <div className="flex w-full lg:flex-row flex-col">
+          <div className="lg:w-[24%] sm:block flex flex-col w-[100%] sm:justify-start justify-center sm:items-start items-center">
             <img
               src={MixMobileLogo}
               className="w-[150px]"
@@ -19,7 +19,7 @@ export default function index({ mode }) {
             />
 
             <span
-              className={`text-white font-medium mt-5 inline-block text-[14px]`}
+              className={`w-[280px] text-white font-medium mt-5 inline-block text-[14px] sm:text-start text-center`}
             >
               Eng so'nggi smartfonlarni eng yaxshi narxlarda olish uchun
               ishonchli manbangiz.
@@ -28,8 +28,8 @@ export default function index({ mode }) {
 
           {/* Footer Links */}
           <div>
-            <ul className="flex gap-40">
-              <li className="flex flex-col gap-1">
+            <ul className="sm:flex flex-wrap grid grid-cols-1 lg:gap-40 md:gap-20 gap-10 lg:mt-0 mt-9">
+              <li className="flex flex-col gap-1 sm:text-start text-center">
                 <p className="text-white font-bold mb-4">Tezkor havolalar</p>
                 <a
                   className="font-medium text-gray-400 hover:text-white text-[15px]"
@@ -56,7 +56,7 @@ export default function index({ mode }) {
                   Bog'lanish
                 </a>
               </li>
-              <li className="flex flex-col gap-1">
+              <li className="flex flex-col gap-1 sm:text-start text-center">
                 <p className="text-white font-bold mb-4">Kategoriyalar</p>
                 <a
                   className="font-medium text-gray-400 hover:text-white text-[15px]"
@@ -83,21 +83,21 @@ export default function index({ mode }) {
                   Others
                 </a>
               </li>
-              <li className="flex flex-col gap-1">
+              <li className="flex flex-col gap-1 sm:text-start text-center">
                 <p className="text-white font-bold mb-4">Kontaktlar</p>
                 <a
                   href="tel:+998889803003"
                   target="_blank"
-                  className="flex items-center gap-2"
+                  className="flex items-center gap-2 sm:justify-start justify-center"
                 >
-                  <span>
+                  <span >
                     <CallIcons keng={16} baland={16} color={"white"} />
                   </span>
                   <span className="text-white text-[15px]">+998889803003</span>
                 </a>
                 <a
                   href="https://www.instagram.com/mix.mobile01/"
-                  className="flex items-center gap-2"
+                  className="flex items-center gap-2 sm:justify-start justify-center"
                   target="_blank"
                 >
                   <span>
@@ -109,7 +109,7 @@ export default function index({ mode }) {
                 </a>
                 <a
                   href="https://t.me/mixmobil"
-                  className="flex items-center gap-2"
+                  className="flex items-center gap-2 sm:justify-start justify-center"
                   target="_blank"
                 >
                   <span>
@@ -129,7 +129,7 @@ export default function index({ mode }) {
           <p className="text-gray-400">
             © 2026 Mix Mobile. All rights reserved.
           </p>
-          <span className="flex gap-4 ">
+          <span className="flex gap-4 sm:mt-0 mt-3">
             <a className="text-gray-400 text-[14px] hover:text-white" href="#">
               Privacy Police
             </a>
